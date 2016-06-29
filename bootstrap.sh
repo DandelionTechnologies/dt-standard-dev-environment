b96d933e2a2494c6a5bc3231636c68eef88410a3
Chris <csw11235@gmail.com>
2016-06-28 23:12:19 -0400

    bootstrap creates an html file in the vagrant_shared folder - this allows for easy verification that web access to the machine is working

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,5 @@ if ! [ -L /var/www ]; then
     rm -rf /var/www
     ln -fs /vagrant_shared /var/www
 fi
+
+echo "<html><head><title>hello world</title></head><body><h2>hello world</h2>things are working, at least up to this point</body></html>" > /vagrant_shared/hello_world.html
